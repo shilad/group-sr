153ad9f298abb2fa90c4861704bfcdd3869fe95e
shilad <shilad@gmail.com>
2014-06-01 21:25:24 -0500

    Add ranks into KnownSimGuess

--- src/main/java/org/shilad/groupsr/AlgorithmicDifferenceProbe.java
+++ src/main/java/org/shilad/groupsr/AlgorithmicDifferenceProbe.java
@@ -30,57 +30,26 @@ public class AlgorithmicDifferenceProbe {
     }
 
     private void analyzePairs(Map<String, Map<String, KnownSimGuess>> guessesByPair) {
-
-        // accumulate SR scores for each algorithm
-        Map<String, TDoubleList> algScores = new HashMap<String, TDoubleList>();
-        algScores.put("actual", new TDoubleArrayList());
-        for (Map<String, KnownSimGuess> pairGuesses : guessesByPair.values()) {
-            boolean firstAlg = true;
-            for (String alg : pairGuesses.keySet()) {
-                if (!algScores.containsKey(alg)) {
-                    algScores.put(alg, new TDoubleArrayList());
-                }
-                algScores.get(alg).add(pairGuesses.get(alg).getGuess());
-                if (firstAlg) {
-                    algScores.get("actual").add(pairGuesses.get(alg).getActual());
-                    firstAlg = false;
-                }
-            }
-        }
-
-
-        // create mapping from SR scores to ranks
-        NaturalRanking nr = new NaturalRanking(TiesStrategy.MAXIMUM);
-        Map<String, TDoubleDoubleMap> scoreToRank = new HashMap<String, TDoubleDoubleMap>();
-        for (String alg : algScores.keySet()) {
-            double [] ranks = nr.rank(algScores.get(alg).toArray());
-            scoreToRank.put(alg, new TDoubleDoubleHashMap());
-            for (int i = 0; i < ranks.length; i++) {
-                scoreToRank.get(alg).put(algScores.get(alg).get(i), 1 + ranks.length - ranks[i]);
-            }
-        }
-
         Map<String, Double> pairScores = new HashMap<String, Double>();
         for (String pairKey : guessesByPair.keySet()) {
-            double minRank = Double.MAX_VALUE;
-            double maxRank = -1.0;
+            double minError = Double.MAX_VALUE;
+            double maxError = -1.0;
 
             for (String alg : guessesByPair.get(pairKey).keySet()) {
                 KnownSimGuess g = guessesByPair.get(pairKey).get(alg);
-                double r = scoreToRank.get(alg).get(g.getGuess());
-                minRank = Math.min(minRank, r);
-                maxRank = Math.max(maxRank, r);
+                minError = Math.min(minError, Math.abs(g.getRankError()));
+                maxError = Math.max(maxError, Math.abs(g.getRankError()));
             }
-            pairScores.put(pairKey, maxRank - minRank);
+            pairScores.put(pairKey, maxError - minError);
         }
 
         List<String> ordered = WpCollectionUtils.sortMapKeys(pairScores, true);
         for (int i = 0; i < Math.min(ordered.size(), NUM_RESULTS); i++) {
-            showPair(i, guessesByPair.get(ordered.get(i)), scoreToRank);
+            showPair(i, guessesByPair.get(ordered.get(i)));
         }
     }
 
-    private void showPair(int i, Map<String, KnownSimGuess> guesses, Map<String, TDoubleDoubleMap> scoreToRank) {
+    private void showPair(int i, Map<String, KnownSimGuess> guesses) {
         if (guesses.isEmpty()) return;
         KnownSim ks = guesses.values().iterator().next().getKnown();
         System.out.println(String.format("%d. %s vs. %s:", (i+1), ks.phrase1, ks.phrase2));
@@ -91,8 +60,8 @@ public class AlgorithmicDifferenceProbe {
             System.out.println(String.format(
                     "\t%s: pred_rank=%.2f actual_rank=%.2f guess=%.3f actual=%.3f",
                     alg,
-                    scoreToRank.get(alg).get(g.getGuess()),
-                    scoreToRank.get("actual").get(g.getActual()),
+                    g.getActualRank(),
+                    g.getPredictedRank(),
                     g.getGuess(), g.getActual()));
         }
     }
@@ -143,36 +112,10 @@ public class AlgorithmicDifferenceProbe {
                 System.out.println("\t" + entry.getKey() + ": " + entry.getValue());
             }
             System.out.println("");
-            guesses.put(dir.getName(), fitGuesses(log.getGuesses()));
+            guesses.put(dir.getName(), log.getGuesses());
         }
 
         return guesses;
-    }
-
-    private List<KnownSimGuess> fitGuesses(List<KnownSimGuess> guesses) {
-        SimpleRegression reg = new SimpleRegression();
-        int n = 0;
-        for (KnownSimGuess g : guesses) {
-            if (g.hasGuess()) {
-                reg.addData(g.getGuess(), g.getActual());
-                n++;
-            }
-        }
-        if (n < 5) {
-            return guesses;
-        }
-        List<KnownSimGuess> fitted = new ArrayList<KnownSimGuess>();
-        for (KnownSimGuess g : guesses) {
-            if (g.hasGuess()) {
-                fitted.add(new KnownSimGuess(
-                        g.getKnown(),
-                        reg.getIntercept() + reg.getSlope() * g.getGuess()
-                ));
-            } else {
-                fitted.add(g);
-            }
-        }
-        return fitted;
     }
 
     public static void main(String args[]) throws IOException, ParseException {
